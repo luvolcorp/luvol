58d4e27b33bb539fd4a79bbe03a3431c58875969
luvolcorp <luvolcorp@luvol.co.kr>
2025-06-05 14:00:43 +0900

    update

--- as.js
+++ as.js
@@ -13,9 +13,8 @@ async function handleSubmit(event) {
     formData.append('mimeType', file.type);
     formData.append('filename', file.name);
 
-		['name', 'email', 'phone', 'product', 'serial',
-		'issue_category', 'issue_detail', 'issue_subdetail', 'issue_description',
-		'address', 'address_detail'
+		['name', 'phone', 'address', 'address_detail', 'product', 'color', 'serial',
+		'issue_category', 'issue_detail', 'issue_subdetail', 'issue_description'
 		].forEach(id => {
 			formData.append(id, form[id].value);
 		});
@@ -41,6 +40,7 @@ async function handleSubmit(event) {
   reader.readAsDataURL(file);
 }
 
+// 이슈 카테고리, 상세, 하위 상세 매핑
 const issueMap = {
   '전원 문제': {
     '전원이 안 켜짐': ['충전 중 무반응', '전원 버튼 눌러도 반응 없음'],
@@ -90,4 +90,38 @@ function updateSubDetail() {
       subdetailSelect.appendChild(opt);
     });
   }
+}
+
+// 제품 모델과 색상 매핑
+const colorMap = {
+  "RS-500": ["블랙", "화이트"],
+  "FS-500": ["메이플", "화이트"],
+  "PS-400": ["실버", "블랙"],
+  "PS-300": ["블랙&화이트"],
+  "DR-500": ["블랙&화이트"]
+};
+
+document.getElementById('product').addEventListener('change', function () {
+  const model = this.value;
+  const colorSelect = document.getElementById('color');
+  colorSelect.innerHTML = '<option value="">색상을 선택하세요</option>';
+  if (colorMap[model]) {
+    colorMap[model].forEach(color => {
+      const opt = document.createElement('option');
+      opt.value = color;
+      opt.textContent = color;
+      colorSelect.appendChild(opt);
+    });
+  }
+});
+
+// 주소입력
+function execDaumPostcode() {
+  new daum.Postcode({
+    oncomplete: function(data) {
+      const fullAddr = data.roadAddress || data.jibunAddress;
+      document.getElementById("address").value = fullAddr;
+      document.getElementById("address_detail").focus();
+    }
+  }).open();
 }
